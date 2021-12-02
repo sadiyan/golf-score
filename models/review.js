0232d39a83174fbb1b4755f86cb2f712c364ea36
Bora Lee <bbora1119@gmail.com>
2021-12-01 21:00:10 -0500

    work in models

---
+++ models/review.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+
+class Review extends Model {
+  
+Review.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      comment: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      created_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+            },
+        },
+    course_id: {
+        type: DataTypes.INTEGER,
+        references: {
+        model: 'course',
+        key: 'id',
+        },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'course',
+    }
+  );
+  
+  module.exports = Review;
