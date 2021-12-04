37d2710d63a465da7d3dde25b56bb65a7d771af0
sadiyan <sadiy4n@gmail.com>
2021-12-04 11:29:20 -0500

    userRoutes, dashboardRoutes and homeRoutes updated

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Course, Score, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/dashboard', async (req, res) => {
+   try {
+      const newCourse = await Course.findAll({
+         include: [{
+            model: Score,
+         }]
+      })
+      const courses = newCourse.map((course) => course.get({ plain: true }));
+      res.render('dashboard', {courses})
+   }
+
+   catch (err) {
+      res.status(400).json(err);
+   }
+});
+
+router.get ('/dashboard', async (req, res) )
