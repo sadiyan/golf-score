c642ba569b4b191c8c6bcfc64263d37e7a423205
Baylor <baylorhinshaw21@gmail.com>
2021-12-06 18:47:44 -0500

    added to my board

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -34,4 +34,23 @@ router.get('/dashboard', async (req, res) => {
    }
 });
 
+router.get('/board', (req, res) => {
+   try {
+      res.render('myboard')
+   }
+   catch (err) {
+      res.status(400).json(err);
+   }
+ });
+
+router.get('/login', (req, res) => {
+   // If the user is already logged in, redirect the request to another route
+   if (req.session.logged_in) {
+     res.redirect('/dashboard');
+     return;
+   }
+ 
+   res.render('login');
+ });
+
 module.exports = router
